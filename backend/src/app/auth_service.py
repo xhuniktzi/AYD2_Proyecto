5f99c44c599ade55c9bd31fb4a6e7e651409be56
Xhunik Miguel <xhuniktzi@gmail.com>
2024-09-17 13:54:33 -0600

    Validar correctamente el formato de fecha nacimiento en registro

--- backend/src/app/auth_service.py
+++ backend/src/app/auth_service.py
@@ -1,10 +1,9 @@
-
 from flask import Blueprint, request, jsonify
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask_jwt_extended import create_access_token
 from app.models import TokenCheckin, User, db
 from app.utils import generar_nombre_usuario, read_config, generar_token_verificacion, obtener_fechas
-
+from datetime import datetime  # Import necesario para manejar fechas
 
 auth = Blueprint('auth', __name__)
 
@@ -28,25 +27,29 @@ def auth_login():
         return jsonify(access_token=access_token), 200
     else:
         return jsonify({"msg": "Bad credentials"}), 401
-    
+
 @auth.route('/register', methods=['POST'])
 def auth_register():
-    
-    
     data = request.get_json()
     fullname = data['fullname']
-    fecha_nac = data['fecha_nac']
+    fecha_nac_str = data['fecha_nac']  # Fecha de nacimiento en formato de cadena
     password = data['password']
     genero = data['genero']
     email = data['email']
     phone = data['phone']
+
+    # Convertir la fecha de nacimiento al formato datetime
+    try:
+        fecha_nac = datetime.strptime(fecha_nac_str, '%d/%m/%Y')
+    except ValueError:
+        return jsonify({"msg": "Formato de fecha inválido. Utiliza dd/mm/yyyy"}), 400
 
     new_user = User()
     new_user.username = generar_nombre_usuario()
     new_user.fullname = fullname
     new_user.fecha_nac = fecha_nac
     new_user.password = generate_password_hash(password)
-    new_user.genero_id = genero # 0 is nothing, 1 is female, 2 is male
+    new_user.genero_id = genero  # 0: ninguno, 1: femenino, 2: masculino
     new_user.email = email
     new_user.phone_number = phone
     new_user.state_id = int(read_config("user-not-checkin"))
@@ -58,7 +61,7 @@ def auth_register():
     new_token.token = generar_token_verificacion()
     new_token.user_id = int(new_user.id)
     new_token.created_at, new_token.expires_at = obtener_fechas()
-    
+
     db.session.add(new_token)
     db.session.commit()
 
