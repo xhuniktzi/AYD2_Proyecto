1e6eac873b2f2d2255e1678fb3182b2fcbd39d16
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2024-10-07 13:36:50 -0600

    [ADD]Frontend Conductor

---
+++ frontend/models/IDriverInfoUser.ts
@@ -0,0 +1,10 @@
+export default interface IDriverInfoUser {
+    id: number;
+    fullname: string;
+    username: string;
+    fecha_nac: string;
+    genero: string;
+    email: string;
+    phone_number: string;
+  }
+  
--- frontend/models/ITripInfo.ts
+++ frontend/models/ITripInfo.ts
@@ -3,6 +3,10 @@ export interface ITripInfo {
     destination: string;
     tarifa: number;
     status: string; // "Driver assigned" or "Driver not assigned yet"
+    user_fullname?:string;//Optional
+    email?: string;
+    phone_number?:string;
+    genero?:string;
     driver_name?: string; // Optional, available only when driver is assigned
     car_brand?: string; // Optional
     car_model_year?: number; // Optional
