18c987cb7be13eedafc48a985d7e4b96abbec7c2
ZenaidaChacon <58494863+ZenaidaChacon@users.noreply.github.com>
2024-10-06 23:42:58 -0600

    [ADD] templates and backed conductores

---
+++ frontend/app/driver/layout.tsx
@@ -0,0 +1,30 @@
+import { Metadata } from "next";
+import { Button } from "@/components/ui/button";
+
+export const metadata: Metadata = {
+  title: "Conductor - QNave",
+};
+
+export default function AssistantLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <div className="h-screen w-screen">
+      <nav className="px-5 h-14 flex items-center border-b-2">
+        <ul className="flex space-x-5">
+          <li>
+            <Button variant={"ghost"} asChild>
+              <a href="list_trip">Lista de Viajes</a>
+            </Button>
+          </li>
+          <li>
+            <Button variant={"ghost"} asChild>
+              <a href="reports">Reportar Problema</a>
+            </Button>
+          </li>
+        </ul>
+      </nav>
+      <div className="p-10">{children}</div>
+    </div>
+  );
+}
