0a234cc00dbf1757d949a3f7b8cf0dbfc0339e2c
Xhunik Miguel <xhuniktzi@gmail.com>
2024-10-27 17:58:05 -0600

    fix database name

--- rundatabase.sh
+++ rundatabase.sh
@@ -7,7 +7,7 @@ docker run -d \
   --name postgres_service \
   -e POSTGRES_USER=sa \
   -e POSTGRES_PASSWORD=database123! \
-  -e POSTGRES_DB=ubefake \
+  -e POSTGRES_DB=uberfake \
   -p 5432:5432 \
   --network app-network \
   -v db-volume:/var/lib/postgresql/data \
